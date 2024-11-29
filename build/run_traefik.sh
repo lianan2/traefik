fe196197da8b5be3fb982dc514e8d8fc94b95858
lianan <li_a_nan2@126.com>
2024-11-29 07:12:20 +0000

    update

--- build/run_traefik.sh
+++ build/run_traefik.sh
@@ -3,8 +3,8 @@
 kubectl create ns traefik
 
 # 创建traefik toml配置文件的configmap, 用于挂载
-kubectl -n traefik create configmap traefik-config --from-file=conf/traefik.toml
-kubectl -n traefik create configmap traefik-config-dynamic --from-file=conf/dynamic.toml
+kubectl -n traefik create configmap traefik-config --from-file=traefik/conf/traefik.toml
+#kubectl -n traefik create configmap traefik-config-dynamic --from-file=traefik/conf/dynamic.toml
 
 # 安装traefik crd, 创建rbac权限
 kubectl apply -f traefik/crd.yaml -f traefik/crd-rbac.yaml
--- clear.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete ns traefik
-kubectl get crd | grep traefik | awk '{print $1}' | xargs kubectl delete crd
-kubectl get clusterrole | grep traefik | awk '{print $1}' | xargs kubectl delete clusterrole
-kubectl get clusterrolebinding | grep traefik | awk '{print $1}' | xargs kubectl delete clusterrolebinding
-kubectl get ingressclass | grep traefik | awk '{print $1}' | xargs kubectl delete ingressclass
-kubectl -nkuas get ingress | grep traefik | awk '{print $1}' | xargs kubectl -nkuas delete ingress
-kubectl -nkuas get svc | grep traefik | awk '{print $1}' | xargs kubectl delete svc
